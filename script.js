75864de7ea234fa3786d5247b1a7217b84ce2b83
Unknown <m.brossard@live.fr>
2018-04-19 11:36:08 +0200

    ajout bouton et style de l'application

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+$(document).ready(function(){
+  
+});
+
+
+
+
 window.fetch('http://api.openweathermap.org/data/2.5/weather?q=Nantes&lang=fr&units=metric&appid=3a3255d86f5d921cd5f2526dcdd05977')
   .then(res => res.json())
   .then(resjson => console.log(resjson))
