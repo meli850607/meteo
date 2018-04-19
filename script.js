a351e17cfedb387306c30abc4b20ebdd5fd14713
Unknown <m.brossard@live.fr>
2018-04-19 12:15:33 +0200

    ajout widget météo

--- script.js
+++ script.js
@@ -1,13 +1,36 @@
-$(document).ready(function(){
-  
+$(document).ready(function() {
+  $("#submitWeather").click(function() {
+    var city = $("#city").val();
+    if (city != '') {
+
+      $.ajax({
+        url: "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&lang=fr&units=metric" + "&appid=3a3255d86f5d921cd5f2526dcdd05977",
+        type: "GET",
+        dataType: "jsonp",
+        success: function(data) {
+          var widget = show(data);
+          $("#show").html(widget);
+          $("#city").val('');
+        }
+      });
+
+    } else {
+      $("#error").html('cette case ne peut pas être vide');
+    }
+
+  });
+
 });
 
+function show(data) {
+  return "<h3><strong>temps</strong>: " + data.weather[0].main + "</h3>" +
+          "<h3><strong>temps</strong>: " + data.weather[0].description + "</h3>"; 
+}
 
 
-
-window.fetch('http://api.openweathermap.org/data/2.5/weather?q=Nantes&lang=fr&units=metric&appid=3a3255d86f5d921cd5f2526dcdd05977')
+/*window.fetch('http://api.openweathermap.org/data/2.5/weather?q=Nantes&lang=fr&units=metric&appid=3a3255d86f5d921cd5f2526dcdd05977')
   .then(res => res.json())
   .then(resjson => console.log(resjson))
-
+*/
 
 //resjson.main.name = document.getElementsByClassName('ville')
