89dd093e82b59b1d0ed7828219e487f6fd615c9b
Unknown <m.brossard@live.fr>
2018-04-20 10:10:23 +0200

    correction du problème d'affichage météo

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-"user strict"
+"use strict"
 $(document).ready(function() {
-  $("#submitWeather").click(function() {
+  $('#submitWeather').click(function() {
     var city = $("#city").val();
     if (city != '') {
 
@@ -23,9 +23,20 @@ $(document).ready(function() {
 
 });
 
-function show(data) {
-    return "<h3><strong>Temps</strong>: "+ data.weather[0].main +"</h3>" +
-           "<h3><strong>Temps</strong>: "+ data.weather[0].description +"</h3>";
+function show(data){
+          return "<h3 style='font-size:40px; font-weight: bold;' class='text-center'>La Météo à: " +
+            data.name + ", " + data.sys.country + "</h3>" +
+
+    "<h3><strong>Météo</strong>: "+ data.weather[0].main +"</h3>" +
+    "<h3><strong>Description</strong>: "+ data.weather[0].description +"</h3>" +
+    "<h3><strong>Température</strong>: " + data.main.temp + "&deg;C</h3>" +
+    "<h3><strong>Pression</strong>: " + data.main.pressure + "hPa</h3>" +
+    "<h3><strong>Humidité</strong>: " + data.main.humidity + "%</h3>" +
+    "<h3><strong>Température maximale</strong>: " + data.main.temp_max + "&deg;C</h3>" +
+    "<h3><strong>Température minimale</strong>: " + data.main.temp_min + "&deg;C</h3>" +
+    "<h3><strong>Vitesse du vent</strong>: " + data.wind.speed + "m/s</h3>" +
+    "<h3><strong>Direction du vent</strong>: " + data.wind.deg + "&deg;C</h3>";
+
 }
 
 //à continuer avec vidéo : https://www.youtube.com/watch?v=6imiFFeDIzE
