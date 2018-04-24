f9537517509e6da48e2903892aa7f5516a4a038a
Unknown <m.brossard@live.fr>
2018-04-24 16:36:44 +0200

    ajout condition images à finir

--- script.js
+++ script.js
@@ -27,6 +27,7 @@ function show(data){
           return "<h3 style='font-size:40px; font-weight: bold;' class='text-center'>La Météo à : " +
             data.name + " , " + data.sys.country + "</h3>" +
 
+    "<h3><strong>Weather</strong>"+" : " + data.weather[0].main +"</h3>" +
     "<h3><strong>Météo</strong>"+" : " + data.weather[0].description +"</h3>" +
     "<h3><strong>Température</strong>"+" : " + data.main.temp + "&deg;C</h3>" +
     "<h3><strong>Pression</strong>"+" : " + data.main.pressure + "hPa</h3>" +
@@ -36,15 +37,29 @@ function show(data){
     "<h3><strong>Vitesse du vent</strong>"+" : " + data.wind.speed + "m/s</h3>" +
     "<h3><strong>Direction du vent</strong>"+" : " + data.wind.deg + "&deg;</h3>";
 
-}
-switch(data.weather.main){
+
+var weather = data.weather.main;
+switch(weather){
   case 'Clear':
-  document.getElementByClassName('container') = background-image = url(img/cielbleu.jpg);
-  break;
+    document.getElementByClassName('container').style.backgroundImage = url(img/cielbleu.jpg);
+    break;
+  case 'Clouds':
+    document.getElementByClassName('container').style.backgroundImage = url(img/nuageux.jpg);
+    break;
+  case 'Thunderstorm':
+    document.getElementByClassName('container').style.backgroundImage = url(img/orage.jpg);
+    break;
+  case 'Rain':
+    document.getElementByClassName('container').style.backgroundImage = url(img/pluie.jpeg);
+    break;
+  case 'Drizzle':
+    document.getElementByClassName('container').style.backgroundImage = url(img/pluie.jpeg);
+    break;
+  default:
+    document.getElementByClassName('container').style.backgroundImage = url(img/plage.jpg);
+    break;
+  }
 }
-/*var i = data.weather[0].description;
-if (i === "ciel degagé"){
-  document.getElementById("cielbleu").innerHTML =
-}*/
+
 
 //http://api.openweathermap.org/data/2.5/weather?q=Nantes&lang=fr&units=metric&appid=3a3255d86f5d921cd5f2526dcdd05977
